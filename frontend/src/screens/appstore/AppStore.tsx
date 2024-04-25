a0e66ad4c4ab4aa08d0e70808e3176fad0fd13d8
René Aaron <rene@twentyuno.net>
2024-04-26 00:05:31 +0200

    feat: how to connect

--- frontend/src/screens/appstore/AppStore.tsx
+++ frontend/src/screens/appstore/AppStore.tsx
@@ -2,6 +2,7 @@ import { CirclePlus } from "lucide-react";
 import AppHeader from "src/components/AppHeader";
 import SuggestedApps from "src/components/SuggestedApps";
 import { Button } from "src/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "src/components/ui/dialog";
 
 function AppStore() {
   return (
@@ -11,6 +12,27 @@ function AppStore() {
         description="Apps that you can connect your wallet into"
         contentRight={
           <>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="secondary" className="hidden sm:flex">
+                  How to connect to apps?
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>
+                    How to connect to apps
+                  </DialogTitle>
+                  <DialogDescription>
+                    Open the app you want to pair and scan this QR code to connect.
+                  </DialogDescription>
+                  <div className="flex flex-row justify-center pt-5">
+                    To pair with NWC (Nostr Wallet Connect), open the target app and navigate to its settings. Here, you should find an option for NWC.
+                  </div>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+
             <a
               href="https://form.jotform.com/232284367043051"
               target="_blank"
