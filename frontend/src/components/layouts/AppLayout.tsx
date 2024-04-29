db38501de096d8548c45b7ef1bfaa7e2265c4ed3
René Aaron <rene@twentyuno.net>
2024-04-29 16:40:25 +0200

    fix: global banner

--- frontend/src/components/layouts/AppLayout.tsx
+++ frontend/src/components/layouts/AppLayout.tsx
@@ -40,6 +40,7 @@ export default function AppLayout() {
   const { data: albyMe } = useAlbyMe();
   const { data: csrf } = useCSRF();
   const { toast } = useToast();
+  const { data: info } = useInfo();
   const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
   const location = useLocation();
   const navigate = useNavigate();
@@ -129,65 +130,21 @@ export default function AppLayout() {
     );
   }
 
-  return (
-    <div className="font-sans grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex-1">
-            <nav className="grid items-start px-2 py-2 text-sm font-medium lg:px-4">
-              <div className="p-3 ">
-                <Link to="/" className="font-semibold text-xl">
-                  <span className="">Alby Hub</span>
-                </Link>
-              </div>
-              <MainMenuContent />
-            </nav>
-          </div>
-          <div className="flex flex-col">
-            <MainNavSecondary />
-            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 gap-3 border-t border-border justify-between">
-              <div className="grid grid-flow-col gap-2">
-                <Avatar className="h-8 w-8">
-                  <AvatarImage src={albyMe?.avatar} alt="@satoshi" />
-                  <AvatarFallback>SN</AvatarFallback>
-                </Avatar>
-                <Link
-                  to="#"
-                  className="font-semibold text-lg whitespace-nowrap overflow-hidden text-ellipsis"
-                >
-                  {albyMe?.name ?? albyMe?.email}
-                </Link>
-              </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" size="icon">
-                    <CaretUpIcon />
-                  </Button>
-                </DropdownMenuTrigger>
-                <UserMenuContent />
-              </DropdownMenu>
-            </div>
-          </div>
+  return (<>
+    <div className="font-sans min-h-screen w-full flex flex-col">
+      {info && !info.onboardingCompleted &&
+        <div className="p-2 font-medium text-sm text-center bg-foreground text-background">
+          To start using Alby Hub, migrate your funds or open your first lightning channel!{" "}
+          <Link to="/channels/new" className="underline hover:no-underline">
+            Start here
+          </Link>
         </div>
-      </div>
-      <main className="flex flex-col">
-        <header className="md:hidden flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 justify-between">
-          <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent
-              side="left"
-              className="flex flex-col justify-between max-h-screen"
-            >
-              <nav className="grid gap-2 text-lg font-medium">
+      }
+      <div className="flex-1 h-full grid md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex-1">
+              <nav className="grid items-start px-2 py-2 text-sm font-medium lg:px-4">
                 <div className="p-3 ">
                   <Link to="/" className="font-semibold text-xl">
                     <span className="">Alby Hub</span>
@@ -195,31 +152,85 @@ export default function AppLayout() {
                 </div>
                 <MainMenuContent />
               </nav>
-              <div className="align-bottom">
-                <MainNavSecondary />
-              </div>
-            </SheetContent>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Link
-                  to="#"
-                  className="grid grid-flow-col gap-2 font-semibold text-lg whitespace-nowrap overflow-hidden text-ellipsis"
-                >
+            </div>
+            <div className="flex flex-col">
+              <MainNavSecondary />
+              <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6 gap-3 border-t border-border justify-between">
+                <div className="grid grid-flow-col gap-2">
                   <Avatar className="h-8 w-8">
                     <AvatarImage src={albyMe?.avatar} alt="@satoshi" />
                     <AvatarFallback>SN</AvatarFallback>
                   </Avatar>
-                </Link>
-              </DropdownMenuTrigger>
-              <UserMenuContent />
-            </DropdownMenu>
-          </Sheet>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-8">
-          <Outlet />
+                  <Link
+                    to="#"
+                    className="font-semibold text-lg whitespace-nowrap overflow-hidden text-ellipsis"
+                  >
+                    {albyMe?.name ?? albyMe?.email}
+                  </Link>
+                </div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <CaretUpIcon />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <UserMenuContent />
+                </DropdownMenu>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+        <main className="flex flex-col">
+          <header className="md:hidden flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 justify-between">
+            <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
+              <SheetTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="shrink-0 md:hidden"
+                >
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent
+                side="left"
+                className="flex flex-col justify-between max-h-screen"
+              >
+                <nav className="grid gap-2 text-lg font-medium">
+                  <div className="p-3 ">
+                    <Link to="/" className="font-semibold text-xl">
+                      <span className="">Alby Hub</span>
+                    </Link>
+                  </div>
+                  <MainMenuContent />
+                </nav>
+                <div className="align-bottom">
+                  <MainNavSecondary />
+                </div>
+              </SheetContent>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Link
+                    to="#"
+                    className="grid grid-flow-col gap-2 font-semibold text-lg whitespace-nowrap overflow-hidden text-ellipsis"
+                  >
+                    <Avatar className="h-8 w-8">
+                      <AvatarImage src={albyMe?.avatar} alt="@satoshi" />
+                      <AvatarFallback>SN</AvatarFallback>
+                    </Avatar>
+                  </Link>
+                </DropdownMenuTrigger>
+                <UserMenuContent />
+              </DropdownMenu>
+            </Sheet>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-8">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div></>
   );
 }
 
