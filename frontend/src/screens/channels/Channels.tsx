09a1db88903d64f60cbb17bb306e9cd115ec628c
René Aaron <rene@twentyuno.net>
2024-04-30 19:37:18 +0200

    fix: remove obsolete button

--- frontend/src/screens/channels/Channels.tsx
+++ frontend/src/screens/channels/Channels.tsx
@@ -22,7 +22,7 @@ import {
   CardHeader,
   CardTitle,
 } from "src/components/ui/card.tsx";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "src/components/ui/dialog.tsx";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "src/components/ui/dialog.tsx";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -32,8 +32,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "src/components/ui/dropdown-menu.tsx";
-import { Input } from "src/components/ui/input.tsx";
-import { Label } from "src/components/ui/label.tsx";
 import {
   Table,
   TableBody,
@@ -408,37 +406,6 @@ export default function Channels() {
           </CardFooter>
         </Card>
       </div>
-
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline">Edit Profile</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="username" className="text-right">
-                Username
-              </Label>
-              <Input id="username" value="@peduarte" className="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
 
       <Table>
         <TableHeader>
