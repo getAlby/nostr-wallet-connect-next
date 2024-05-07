107621f4c076b794c6ba77675acd41b459ed4ee5
Roland Bewick <roland.bewick@gmail.com>
2024-05-07 16:54:53 +0700

    feat: add node status debug api call

--- ldk.go
+++ ldk.go
@@ -997,3 +997,9 @@ func deleteOldLDKLogs(logger *logrus.Logger, ldkLogDir string) {
 		}
 	}
 }
+
+func (ls *LDKService) GetNodeStatus(ctx context.Context) (nodeStatus *lnclient.NodeStatus, err error) {
+	return &lnclient.NodeStatus{
+		InternalNodeStatus: ls.node.Status(),
+	}, nil
+}
