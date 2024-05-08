a7823993d00d51a24759d45e8e340e24e5dfff36
René Aaron <100827540+reneaaron@users.noreply.github.com>
2024-05-08 13:20:42 +0200

    Update scripts/update.sh
    Co-authored-by: Michael Bumann <hello@michaelbumann.com>

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,7 +5,9 @@ sudo systemctl stop albyhub
 
 cd /opt/albyhub
 rm -rf albyhub-backup
+mkdir albyhub-backup
 mv app albyhub-backup
+cp -r data albyhub-backup
 wget https://nightly.link/getalby/nostr-wallet-connect-next/workflows/package-raspberry-pi/master/nostr-wallet-connect.zip
 
 unzip nostr-wallet-connect.zip -d app
