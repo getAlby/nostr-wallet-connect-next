08ba5dfe6231407930c5188183a5691cbab4e4bd
René Aaron <rene@twentyuno.net>
2024-05-10 12:01:00 +0200

    fix: buttons & titles

--- frontend/src/screens/channels/NewChannel.tsx
+++ frontend/src/screens/channels/NewChannel.tsx
@@ -23,7 +23,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "src/components/ui/select";
-import { Step, StepItem, Stepper } from "src/components/ui/stepper";
+import { Step, StepItem, Stepper, useStepper } from "src/components/ui/stepper";
 import { useChannelPeerSuggestions } from "src/hooks/useChannelPeerSuggestions";
 import { useInfo } from "src/hooks/useInfo";
 import { cn, formatAmount } from "src/lib/utils";
@@ -128,10 +128,32 @@ function NewChannelInternal({ network }: { network: Network }) {
   }
 
   const steps = [
-    { label: "Step 1" },
-    { label: "Step 2" },
-    { label: "Step 3" },
+    { label: "Channel Size" },
+    { label: "Funding Method" },
+    { label: "Channel Peer" },
+    { label: "Deposit Funds" },
+    { label: "Open the Channel" },
   ] satisfies StepItem[];
+
+  const StepButtons = () => {
+    const { nextStep, prevStep, isLastStep, isOptionalStep, isDisabledStep } =
+      useStepper()
+    return (
+      <div className="w-full flex gap-2 mb-4">
+        <Button
+          disabled={isDisabledStep}
+          onClick={prevStep}
+          size="sm"
+          variant="secondary"
+        >
+          Prev
+        </Button>
+        <Button size="sm" onClick={nextStep}>
+          {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
+        </Button>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -161,6 +183,8 @@ function NewChannelInternal({ network }: { network: Network }) {
                 <div className="h-40 flex items-center justify-center my-2 border bg-secondary text-primary rounded-md">
                   <h1 className="text-xl">Step {index + 1}</h1>
                 </div>
+
+                <StepButtons />
               </Step>
             )
           })}
