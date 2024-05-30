e2840d58042dfe7db5da3c4386503ae8c8bd4af3
pavanjoshi914 <pavanj914@gmail.com>
2024-05-30 16:03:01 +0530

    feat: add commands to run linters via script

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,8 +1,6 @@
 export default {
   "src/**/*.{ts,tsx,json}": ["eslint --fix --max-warnings 0","prettier --write"],
   "platform_specific/**/*.ts": ["prettier --write"],
-  "platform_specific/**/*.{ts,js,json}": ["prettier --write"],
-
   "package.json": ["prettier --write"],
   "src/**/*.ts": () => "tsc --noEmit",
 };
