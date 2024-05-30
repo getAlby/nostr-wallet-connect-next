fa7575210e3cd6ad4186f8842b9982a0da23ff82
pavanjoshi914 <pavanj914@gmail.com>
2024-05-30 15:00:34 +0530

    feat: setup eslint typescript parsers + update rules

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,14 +5,24 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+      'ts-ignore': 'allow-with-description'
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { 'args': 'none' }],
+    "no-console": ["error", { "allow": ['info', 'warn', 'error'] }],
+    "no-constant-binary-expression": 'error'
   },
 }
