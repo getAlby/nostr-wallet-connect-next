cf39134424ec17929303655bf4af4b45065b49f2
René Aaron <rene@twentyuno.net>
2024-06-05 16:09:14 +0200

    fix: flows

--- frontend/src/screens/setup/node/LDKForm.tsx
+++ frontend/src/screens/setup/node/LDKForm.tsx
@@ -1,20 +1,31 @@
+import { wordlist } from "@scure/bip39/wordlists/english";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import Container from "src/components/Container";
 import TwoColumnLayoutHeader from "src/components/TwoColumnLayoutHeader";
 import { Button } from "src/components/ui/button";
 import useSetupStore from "src/state/SetupStore";
 
+import * as bip39 from "@scure/bip39";
+
 export function LDKForm() {
   const navigate = useNavigate();
   const setupStore = useSetupStore();
+  const [searchParams] = useSearchParams();
 
   async function handleSubmit(data: object) {
     setupStore.updateNodeInfo({
       backendType: "LDK",
       ...data,
     });
-    navigate("/setup/import-mnemonic");
+
+    if (searchParams.get("wallet") === "new") {
+      setupStore.updateNodeInfo({
+        mnemonic: bip39.generateMnemonic(wordlist, 128),
+      });
+    }
+
+    navigate("/setup/finish");
   }
 
   function onSubmit(e: React.FormEvent) {
