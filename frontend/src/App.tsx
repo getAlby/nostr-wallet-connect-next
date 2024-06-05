391709add716f67e1339ec268647c46e6aafee4a
René Aaron <rene@twentyuno.net>
2024-06-05 19:52:20 +0200

    fix: add pheonix form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -52,6 +52,7 @@ import { CashuForm } from "src/screens/setup/node/CashuForm";
 import { GreenlightForm } from "src/screens/setup/node/GreenlightForm";
 import { LDKForm } from "src/screens/setup/node/LDKForm";
 import { LNDForm } from "src/screens/setup/node/LNDForm";
+import { PhoenixForm } from "src/screens/setup/node/PhoenixForm";
 
 function App() {
   usePosthog();
@@ -133,6 +134,7 @@ function App() {
                   <Route path="ldk" element={<LDKForm />} />
                   <Route path="lnd" element={<LNDForm />} />
                   <Route path="cashu" element={<CashuForm />} />
+                  <Route path="phoenix" element={<PhoenixForm />} />
                 </Route>
                 <Route path="advanced" element={<SetupAdvanced />} />
                 <Route path="import-mnemonic" element={<ImportMnemonic />} />
