d8c9bbd120d462b750a0da81f0edaca497238df5
Roland Bewick <roland.bewick@gmail.com>
2024-06-08 09:33:57 +0700

    chore: remove unused code

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -76,8 +76,6 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 0.2s ease-out",
-        "collapsible-up": "collapsible-up 0.2s ease-out",
       },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
