ed83198b9be03f92fee59d0088651b1a489d3473
Roland Bewick <roland.bewick@gmail.com>
2024-06-17 21:46:49 +0700

    chore: improve lsp api to return necessary fields to frontend to display order

--- frontend/src/screens/channels/CurrentChannelOrder.tsx
+++ frontend/src/screens/channels/CurrentChannelOrder.tsx
@@ -9,7 +9,6 @@ import {
 } from "src/types";
 
 import { Payment, init } from "@getalby/bitcoin-connect-react";
-import { Invoice } from "@getalby/lightning-tools";
 import { Copy, QrCode, RefreshCw } from "lucide-react";
 import { Link } from "react-router-dom";
 import AppHeader from "src/components/AppHeader";
@@ -599,10 +598,6 @@ function PayLightningChannelOrder({ order }: { order: NewChannelOrder }) {
     });
   }, [channels, csrf, order.amount, order.isPublic, order.lsp]);
 
-  const invoiceAmount =
-    wrappedInvoiceResponse &&
-    new Invoice({ pr: wrappedInvoiceResponse.invoice }).satoshi;
-
   return (
     <div className="flex flex-col gap-5">
       <AppHeader
@@ -613,14 +608,40 @@ function PayLightningChannelOrder({ order }: { order: NewChannelOrder }) {
             : "Please wait, loading..."
         }
       />
-      {(!wrappedInvoiceResponse || !invoiceAmount) && <Loading />}
+      {!wrappedInvoiceResponse && <Loading />}
 
-      {wrappedInvoiceResponse && invoiceAmount && (
+      {wrappedInvoiceResponse && (
         <>
           <div className="max-w-md flex flex-col gap-5">
             <div className="border rounded-lg">
               <Table>
                 <TableBody>
+                  {wrappedInvoiceResponse.outgoingLiquidity > 0 && (
+                    <TableRow>
+                      <TableCell className="font-medium p-3">
+                        Spending Balance
+                      </TableCell>
+                      <TableCell className="text-right p-3">
+                        {new Intl.NumberFormat().format(
+                          wrappedInvoiceResponse.outgoingLiquidity
+                        )}{" "}
+                        sats
+                      </TableCell>
+                    </TableRow>
+                  )}
+                  {wrappedInvoiceResponse.incomingLiquidity > 0 && (
+                    <TableRow>
+                      <TableCell className="font-medium p-3">
+                        Incoming Liquidity
+                      </TableCell>
+                      <TableCell className="text-right p-3">
+                        {new Intl.NumberFormat().format(
+                          wrappedInvoiceResponse.incomingLiquidity
+                        )}{" "}
+                        sats
+                      </TableCell>
+                    </TableRow>
+                  )}
                   <TableRow>
                     <TableCell className="font-medium p-3 flex flex-row gap-1.5 items-center">
                       Fee
@@ -632,22 +653,15 @@ function PayLightningChannelOrder({ order }: { order: NewChannelOrder }) {
                       sats
                     </TableCell>
                   </TableRow>
-                  {invoiceAmount < +order.amount && (
-                    <TableRow>
-                      <TableCell className="font-medium p-3">
-                        Incoming Liquidity
-                      </TableCell>
-                      <TableCell className="text-right p-3">
-                        {new Intl.NumberFormat().format(+order.amount)} sats
-                      </TableCell>
-                    </TableRow>
-                  )}
                   <TableRow>
                     <TableCell className="font-medium p-3">
                       Amount to pay
                     </TableCell>
                     <TableCell className="font-semibold text-right p-3">
-                      {new Intl.NumberFormat().format(invoiceAmount)} sats
+                      {new Intl.NumberFormat().format(
+                        wrappedInvoiceResponse.invoiceAmount
+                      )}{" "}
+                      sats
                     </TableCell>
                   </TableRow>
                 </TableBody>
