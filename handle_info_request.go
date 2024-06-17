d0b587291fe66ffa9e165269a06190f3924468fe
im-adithya <imadithyavardhan@gmail.com>
2024-06-17 14:01:31 +0530

    chore: append info if app has permission

--- handle_info_request.go
+++ handle_info_request.go
@@ -16,49 +16,42 @@ func (svc *Service) HandleGetInfoEvent(ctx context.Context, nip47Request *nip47.
 	}
 
 	hasPermission, _, _ := svc.hasPermission(app, nip47Request.Method, 0)
-	if !hasPermission {
-		// get_info should still return request_methods even without a permission
-		publishResponse(&nip47.Response{
-			ResultType: nip47Request.Method,
-			Result:     responsePayload,
-		}, nostr.Tags{})
-		return
-	}
-
-	svc.logger.WithFields(logrus.Fields{
-		"requestEventNostrId": requestEvent.NostrId,
-		"appId":               app.ID,
-	}).Info("Fetching node info")
-
-	info, err := svc.lnClient.GetInfo(ctx)
-	if err != nil {
+	if hasPermission {
 		svc.logger.WithFields(logrus.Fields{
 			"requestEventNostrId": requestEvent.NostrId,
 			"appId":               app.ID,
-		}).Infof("Failed to fetch node info: %v", err)
+		}).Info("Fetching node info")
 
-		publishResponse(&nip47.Response{
-			ResultType: nip47Request.Method,
-			Error: &nip47.Error{
-				Code:    nip47.ERROR_INTERNAL,
-				Message: err.Error(),
-			},
-		}, nostr.Tags{})
-		return
+		info, err := svc.lnClient.GetInfo(ctx)
+		if err != nil {
+			svc.logger.WithFields(logrus.Fields{
+				"requestEventNostrId": requestEvent.NostrId,
+				"appId":               app.ID,
+			}).Infof("Failed to fetch node info: %v", err)
+
+			publishResponse(&nip47.Response{
+				ResultType: nip47Request.Method,
+				Error: &nip47.Error{
+					Code:    nip47.ERROR_INTERNAL,
+					Message: err.Error(),
+				},
+			}, nostr.Tags{})
+			return
+		}
+
+		network := info.Network
+		// Some implementations return "bitcoin" while NIP47 expects "mainnet"
+		if network == "bitcoin" {
+			network = "mainnet"
+		}
+
+		responsePayload.Alias = info.Alias
+		responsePayload.Color = info.Color
+		responsePayload.Pubkey = info.Pubkey
+		responsePayload.Network = network
+		responsePayload.BlockHeight = info.BlockHeight
+		responsePayload.BlockHash = info.BlockHash
 	}
-
-	network := info.Network
-	// Some implementations return "bitcoin" while NIP47 expects "mainnet"
-	if network == "bitcoin" {
-		network = "mainnet"
-	}
-
-	responsePayload.Alias = info.Alias
-	responsePayload.Color = info.Color
-	responsePayload.Pubkey = info.Pubkey
-	responsePayload.Network = network
-	responsePayload.BlockHeight = info.BlockHeight
-	responsePayload.BlockHash = info.BlockHash
 
 	publishResponse(&nip47.Response{
 		ResultType: nip47Request.Method,
