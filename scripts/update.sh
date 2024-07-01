178b492e32867f9208603bf3fb8eea0bee68c85d
René Aaron <rene@twentyuno.net>
2024-07-01 09:40:07 +0200

    fix: update paths

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,10 +12,10 @@ cp -r data albyhub-backup
 wget https://github.com/getAlby/nostr-wallet-connect-next/releases/latest/download/albyhub-Server-Linux-armv6.tar.bz2
 
 # Extract archives
-tar -xvf albyhub-Server-Linux-armv6.tar.gz
+tar -xvf albyhub-Server-Linux-armv6.tar.bz2
 
 # Cleanup
-rm albyhub-Server-Linux-armv6.tar.gz
+rm albyhub-Server-Linux-armv6.tar.bz2
 
 sudo systemctl start albyhub
 
